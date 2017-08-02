657ed93f7bf44bc159e551071c8020a3d5813189
boxcarbranding <jbrito08@gmail.com>
2017-08-01 21:18:17 -0500

    summmary
    desc

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Distiller Design Co.</title>
+  <meta name="description" content="Distiller Design Co. web development and logo design">
+  <meta name="author" content="Distiller Design Co.">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" type="text/css" href="customcss.css">
+    
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    </head>
+
+<body>
+    
+ <div class="container-fluid h-100">
+    <div class="row h-100">
+        <div class="col-sm-6 col-2 backg text-white d-flex align-items-center justify-content-center hidden-xs-down" id="left">
+            <div class="hidden-md-down"><center><img src="/images/DistillerDesignCo-Logo-White.png" width="35%"></center>
+            
+                </div>
+            
+            <div class="hidden-lg-up hidden-xs-down"><center><img src="/images/DistillerDesignCo-Logo-White.png" width="60%"></center></div>
+            
+            <div class="menuu hidden-sm-down"><a href="/">ABOUT</a>  -  <a href="/">CONTACT</a>  -  <a href="/">SOCIAL</a></div>
+        </div><!--end of left side-->
+        <div class="col offset-sm-6 ">
+            
+<!--small phone top menu--><div class="row hidden-sm-up fix"><div class="hidden-sm-up backg"><center><img src="/images/DistillerDesignCo-Logo-White.png" width="60%"></center></div></div>
+            
+        <br><br><br><br>
+<!--small phone text--><h3 class="hidden-md-up"><br><br>Serving up smooth and refreshing creative. <br> Bottled in Austin, TX.</h3>
+            <h2 class="pdd hidden-sm-down">Serving up smooth and refreshing creative. <br> Bottled in Austin, TX.</h2>
+            <br><br><br><br>
+<h1 class="hidden-xs-down">Handcrafted <br>Brand Identities</h1>
+            <hr align="left" width="50%">
+            
+            <div class="row">
+            <div class="img-responsive one col-xl-6 col-lg-6 col-md-12 col-sm-12"><img src="/images/blackitrain_00000.png">
+<img src="/images/ipump.gif" class="onehuv">           
+                </div>
+            <div class="img-responsive two col-xl-6 col-lg-6 col-md-12 col-sm-12"><img src="/images/lol-black.jpg">
+<img src="/images/lol.jpg" class="twohuv"> 
+                </div>
+                <div class="img-responsive three col-xl-6 col-lg-6 col-md-12 col-sm-12"><img src="/images/rewards-black.jpg">
+<img src="/images/rewards.jpg" class="threehuv"> 
+                </div>
+                <div class="img-responsive four col-xl-6 col-lg-6 col-md-12 col-sm-12"><img src="/images/dqcm-black.jpg">
+<img src="/images/dqcm.jpg" class="fourhuv">
+                </div>
+                <div class="img-responsive five col-xl-6 col-lg-6 col-md-12 col-sm-12"><img src="/images/lafleurAnders-black.jpg">
+<img src="/images/lafleurAnders.jpg" class="fivehuv">
+                </div>
+                <div class="img-responsive six col-xl-6 col-lg-6 col-md-12 col-sm-12"><img src="/images/gfc-black.jpg">
+<img src="/images/gfc.jpg" class="sixhuv">
+                </div>
+                
+                </div>
+            <br><br><br>
+            <h1 class="hidden-xs-down">Crisp, clean <br> web design</h1>
+            <hr align="left" width="50%">
+            
+            <p>BrandingTab
+                Taking ideas and handcrafting them into crisp, refreshing creative.
+                Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
+                raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
+                Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+
+            <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb
+                readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies,
+                forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
+                
+            <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
+                raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
+                Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+
+            <?php include('footer.php'); ?>
+            
+        </div>
+    </div>
+</div>
+    
+    
+    
+        
+   
+ 
+</body>
+</html>
