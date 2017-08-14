0c545d2a5a85f0102ebf4e7a025c54470e3b0641
boxcarbranding <jbrito08@gmail.com>
2017-08-14 18:57:45 -0500

    adding split page
    with sightfood

---
+++ split.php
@@ -0,0 +1,253 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+      <title>Distiller Design Co.</title>
+  
+  <link href="css/bootstrap.css" rel="stylesheet">
+    
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    
+    <link rel="stylesheet" type="text/css" href="newc.css">
+    
+    </head>
+
+<body>
+    <header>
+    <div id="parent"> <center>
+        <img src="/images/sightFoodWhite-12.png" width="350px">
+        </center>
+        </div>
+</header>
+<nav class="navbar navcolor1 " data-toggle="affix">
+    <div class="row"><div class="logohold">
+        <a class="navbar-brandX" href="#4"><img src="/images/burger-11.png" width="90px"></a></div>
+    <div class="regmen hidden-sm-down">  ABOUT  WORK  CONTACT</div>
+    <div class="mdmd">
+    <button class="navbar-toggler hidden-md-up col-xs-2" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+        ☰
+    </button>
+    </div>
+    </div>
+    <div class="collapse navbar-toggleable-xs" id="collapsingNavbar">
+
+        <ul class="nav navbar-nav pull-right">
+           
+            <li class="nav-item">
+                <a class="nav-link" href="#1">ABOUT</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#2">WORK</a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link" href="#3">CONTACT</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container-fluid" id="main">
+    <br>&nbsp;<br>&nbsp;<br><br>&nbsp;<br>
+    <h1>Recent Work</h1>
+    <span class="fourp">Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque lacus neque, imperdiet sed dignissim ac, tincidunt vel risus.</span><br>
+    
+    <br>&nbsp;<br>&nbsp;<br><br>&nbsp;
+    
+    <div class="row">
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-4">
+            <img src="http://via.placeholder.com/350x215">
+        </div>
+        
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-4">
+            <img src="http://via.placeholder.com/350x215">
+        </div>
+        
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-4">
+            <img src="http://via.placeholder.com/350x215">
+        </div>
+        
+    
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-4">
+            <img src="http://via.placeholder.com/350x215">
+        </div>
+        
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-4">
+            <img src="http://via.placeholder.com/350x215">
+        </div>
+        
+    
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-4">
+            <img src="http://via.placeholder.com/350x215">
+        </div>
+        
+    </div>
+    
+    
+    
+    <div class="row">
+        
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-3">
+            <img src="/images/iTrain-Logo.jpg">
+        </div>
+        
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-3">
+            <img src="/images/lol-Logo.jpg">
+        </div>
+        
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-3">
+            <img src="/images/DQCM-Logo.jpg">
+        </div>
+        
+    
+        <div class="piece col-xs-12 col-sm-12 col-md-6 col-lg-3">
+            <img src="/images/gfc-Logo.jpg">
+        </div>
+        </div>
+       
+    
+    <br>&nbsp;<br>&nbsp;<br>
+    
+    <div class="row">
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+    </div>
+     <div class="row">
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+    </div>
+     <div class="row">
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4">
+            <div class="card card-outline-primary">
+               <div class="card-block">
+               <h3 class="card-title">Card</h3>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+ 
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <script js>
+    $(document).ready(function() {
+
+  var toggleAffix = function(affixElement, scrollElement, wrapper) {
+  
+    var height = affixElement.outerHeight(),
+        top = wrapper.offset().top;
+    
+    if (scrollElement.scrollTop() >= top){
+        wrapper.height(height);
+        affixElement.addClass("affix");
+    }
+    else {
+        affixElement.removeClass("affix");
+        wrapper.height('auto');
+    }
+      
+  };
+  
+
+  $('[data-toggle="affix"]').each(function() {
+    var ele = $(this),
+        wrapper = $('<div></div>');
+    
+    ele.before(wrapper);
+    $(window).on('scroll resize', function() {
+        toggleAffix(ele, $(this), wrapper);
+    });
+    
+    // init
+    toggleAffix(ele, $(window), wrapper);
+  });
+  
+});
+    </script>
+</body>
+</html>
